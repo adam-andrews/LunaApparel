8fedd764439b0c0992b6d1faca42bd0ab9d9a55b
Adam Andrews <andrewsadam07@gmail.com>
2022-11-02 15:43:18 +0000

    Minor performance improvement

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,11 @@
 import Logo from '@images/Logo.svg';
+import Image from 'next/image';
 export default function Footer() {
 	return (
 		<footer className="bg-gray-800 opacity-90 mt-10">
 			<div className="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
 				<div className="mt-8 flex justify-center space-x-6">
-					<img className="h-8 w-auto" src={Logo.src} alt="" />
+					<Image height={32} width={32} src={Logo.src} alt="" />
 				</div>
 				<p className="mt-8 text-center text-base text-white">
 					&copy; {new Date().getFullYear()} Luma apparel, Inc. All rights
